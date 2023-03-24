8de676dab206a817f4d9698c6417ed284fe560ae
viniciusbenicio <vinicius.benicio97@gmail.com>
2023-03-23 22:32:39 -0300

    Versionamento

---
+++ Editor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Modo Editor");
+            Console.WriteLine("-----------");
+            Start();
+        }
+
+        public static void Start()
+        {
+            var file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.WriteLine("-----------");
+            Console.WriteLine("Deseja salvar o arquivo?");
+            Viewer.Show(file.ToString());
+
+        }
+
+    }
+}
